36213e641d785d68057a5455063c95c75b0c6ce1
Rasika Venkatesh <rvenkatesh@ucdavis.edu>
2020-02-12 21:16:42 -0800

    Worked on cheatingai.py and randomai.py

--- players/cheatingai.py
+++ players/cheatingai.py
@@ -3,21 +3,17 @@ from move import Move
 from players.aiplayer import AIPlayer
 from players.player import Player
 
+
 class CheatingAI(AIPlayer):
     def __init__(self, player_num: int, other_players: Iterable["AIPlayer"], blank_character: str) -> None:
         super().__init__(player_num, other_players, blank_character)
+        ship_coords = []
 
     def init_name(self, player_num: int, other_players: List["AIPlayer"]) -> None:
         self.name = f"Cheating AI {player_num}".strip()
 
     def get_move(self) -> "Move":
-        ship_coords = []
-        opponent = AIPlayer.other_players[0]
-        for row in range(opponent.board.num_rows):
-            for col in range(opponent.board.num_cols):
-                if opponent.board[row][col] != opponent.board.blank_character and opponent.board[row][col] != 'X':
-                    ship_coords.append((row, col))
-
-        coord = ship_coords[0]
-        ship_coords.remove(coord)
-        return Move(self, *coord)
+        for row in range(self.opponents[0].board.num_rows):
+            for col in range(self.opponents[0].board.num_cols):
+                if self.opponents[0].board.contents[row][col].contains_ship() and self.opponents[0].board.contents[row][col].has_been_fired_at == False:
+                    return Move(self, row, col)
--- players/randomai.py
+++ players/randomai.py
@@ -1,5 +1,7 @@
 from typing import Iterable, List
 import move
+from board import Board
+from cell import Cell
 import random
 from players.aiplayer import AIPlayer
 
@@ -7,17 +9,15 @@ from players.aiplayer import AIPlayer
 class RandomAI(AIPlayer):
     def __init__(self, player_num: int, other_players: Iterable["AIPlayer"], blank_character: str) -> None:
         super().__init__(player_num, other_players, blank_character)
+        self.ship_coords = []
+        for row in range(self.board.num_rows):
+            for col in range(self.board.num_cols):
+                self.ship_coords.append((row, col))
 
     def init_name(self, player_num: int, other_players: List["AIPlayer"]) -> None:
         self.name = f"Random AI {player_num}".strip()
 
     def get_move(self) -> move.Move:
-        ship_coords = []
-        for row in range(self.board.get_display(hidden=True).num_rows):
-            for col in range(self.board.get_display(hidden=True).num_cols):
-                if self.board.get_display(hidden=True)[row][col] == self.board.blank_char:
-                    ship_coords.append((row,col))
-
-        coord = random.choice(ship_coords)
-        ship_coords.remove(coord)
+        coord = random.choice(self.ship_coords)
+        self.ship_coords.remove(coord)
         return move.Move(self, *coord)
